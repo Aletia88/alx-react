7a89be6be27accf224a7a761ee80583d598198be
Aletia88 <abababmgmgmg@gmail.com>
2023-11-30 07:39:23 +0300

    redux

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/courses.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/courses.js
@@ -1,10 +1,9 @@
 import { normalize, schema } from 'normalizr';
+
 const courses = new schema.Entity('courses');
-
 const coursesNormalizer = (data) => {
-    const normalizedData = normalize(data, courses);
-    return normalizedData.entities.courses
-}
+  const normalizedData = normalize(data, [courses]);
+  return normalizedData.entities.courses;
+};
 
-export default coursesNormalizer
-
+export default coursesNormalizer;
